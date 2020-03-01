d890789bd709407ac2ea906b2979acd189f22875
kl-k <kl.k@163.com>
2020-03-01 17:35:52 +0800

    hello git

---
+++ src/main/java/com/kl/test/Test.java
@@ -0,0 +1,13 @@
+package com.kl.test;
+
+/**
+ * @Author liang
+ * @Date 2020/3/1 17:34
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
